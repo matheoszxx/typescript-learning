5a613a2b5ca433835abd7a10517de706bc068f61
matheus <matheuszxx1@outlook.com>
2023-09-26 17:42:36 -0300

    conceito de herança no js/typescript

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,10 @@
     protected
  */
 
+//subclasses (classe que herda de outra classe "conceito de herança")
+
 class Character {
-    //nome é opcional
+    //nome é opcional e com o private so pode ser acessado dentro da classe
     private name?: string;
     stregth: number;
     skill: number;
@@ -18,11 +20,31 @@ class Character {
         this.stregth = stregth;
         this.skill = skill;
     }
-
+    //método
     attack(): void {
         console.log(`Attack with ${this.stregth} points`);   
     }
 }
 
+//Character: superclass
+//Magican: subclass
+class Magican extends Character {
+    magicPoints: number
+
+    constructor(
+        name: string, 
+        stregth: number, 
+        skill: number, 
+        magicPoint: number){
+
+        //chamando o construtor da classe Character com o super
+        super(name, stregth, skill)
+
+        this.magicPoints = magicPoint;
+    }
+
+}
+
 const p1 = new Character("Luffy", 10, 98);
+const p2 = new Magican("Mago", 9, 30, 100)
 p1.attack()
